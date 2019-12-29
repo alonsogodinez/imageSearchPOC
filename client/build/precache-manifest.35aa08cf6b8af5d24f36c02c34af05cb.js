a738bb50b4d99bbf968d5659f95d225f27d3f6b6
Alonso Godinez <alonxogs@gmail.com>
2019-12-29 15:10:28 -0500

    serve react app as default

---
+++ client/build/precache-manifest.35aa08cf6b8af5d24f36c02c34af05cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396993fa1c06c443f616ba45b64d781f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ade33a6847b76fe3ce39",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d9d17ff392ab456daa27",
+    "url": "/static/js/2.9dab47d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9dab47d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ade33a6847b76fe3ce39",
+    "url": "/static/js/main.d42312dd.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
